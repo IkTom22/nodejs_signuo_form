0f47e36829b133e2472123f759c914d4421fc025
IkTom22 <ikotominaga@gmail.com>
2021-06-28 20:56:16 +1200

    views files added

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const app = express();
 const port = 5000;
 
 //Set Template engine
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 //Navigation
 app.get("", (req, res) => {
   res.render("index");
@@ -12,4 +15,12 @@ app.get("", (req, res) => {
 app.get("/register", (req, res) => {
   res.render("register");
 });
+
+app.post(
+  "/register",
+  [check("username", "This username must be 3+ characters long").exists()],
+  (req, res) => {
+    res.json(req.body);
+  }
+);
 app.listen(port, () => console.log(`App listening on port ${port}`));
